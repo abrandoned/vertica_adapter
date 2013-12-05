92acabe474e48afab132614c74d8f34165218a88
Brian Stien <brian.stien@moneydesktop.com>
2013-12-05 15:36:48 -0700

    Initial spike at implementing a vertica adapter.

--- lib/active_record/connection_adapters/vertica_adapter.rb
+++ lib/active_record/connection_adapters/vertica_adapter.rb
@@ -1,4 +1,5 @@
 require 'active_record/connection_adapters/abstract_adapter'
+require 'arel/visitors/bind_visitor'
 
 module ActiveRecord
   class Base
@@ -43,10 +44,84 @@ module ActiveRecord
 
   module ConnectionAdapters
     class VerticaColumn < Column
+      def initialize(name, default, sql_type = nil, null = true)
+        super(name, self.class.extract_value_from_default(default), sql_type, null)
+      end
+
+      def self.extract_value_from_default(default)
+        case default
+          # Numeric types
+        when /\A\(?(-?\d+(\.\d*)?\)?)\z/
+          $1
+          # Character types
+        when /\A'(.*)'::(?:character varchar|varying|bpchar|text)\z/m
+          $1
+          # Character types (8.1 formatting)
+        when /\AE'(.*)'::(?:character varchar|varying|bpchar|text)\z/m
+          $1.gsub(/\\(\d\d\d)/) { $1.oct.chr }
+          # Binary data types
+        when /\A'(.*)'::bytea\z/m
+          $1
+          # Date/time types
+        when /\A'(.+)'::(?:time(?:stamp)? with(?:out)? time zone|date)\z/
+          $1
+        when /\A'(.*)'::interval\z/
+          $1
+          # Boolean type
+        when 'true'
+          true
+        when 'false'
+          false
+          # Geometric types
+        when /\A'(.*)'::(?:point|line|lseg|box|"?path"?|polygon|circle)\z/
+          $1
+          # Network address types
+        when /\A'(.*)'::(?:cidr|inet|macaddr)\z/
+          $1
+          # Bit string types
+        when /\AB'(.*)'::"?bit(?: varying)?"?\z/
+          $1
+          # XML type
+        when /\A'(.*)'::xml\z/m
+          $1
+          # Arrays
+        when /\A'(.*)'::"?\D+"?\[\]\z/
+          $1
+          # Object identifier types
+        when /\A-?\d+\z/
+          $1
+        else
+          # Anything else is blank, some user type, or some function
+          # and we can't know the value of that, so return nil.
+          nil
+        end
+      end
+    end
+
+    # Deal with Arel visitors so this works with Rails 3.2
+    class BindSubstitution < Arel::Visitors::PostgreSQL
+      include Arel::Visitors::BindVisitor
     end
 
     class VerticaAdapter < AbstractAdapter
+
       ADAPTER_NAME = 'Vertica'.freeze
+
+      NATIVE_DATABASE_TYPES = {
+        :primary_key => "integer not null primary key",
+        :string      => { :name => "varchar", :limit => 255 },
+        :text        => { :name => "varchar", :limit => 15000 },
+        :integer     => { :name => "integer" },
+        :float       => { :name => "float" },
+        :decimal     => { :name => "decimal" },
+        :datetime    => { :name => "datetime" },
+        :timestamp   => { :name => "timestamp" },
+        :time        => { :name => "time" },
+        :date        => { :name => "date" },
+        :binary      => { :name => "bytea" },
+        :boolean     => { :name => "boolean" },
+        :xml         => { :name => "xml" }
+      }
 
       ##
       # Constructor
@@ -55,7 +130,7 @@ module ActiveRecord
         super(connection)
         @connection_options, @config = connection_options, config
         @quoted_column_names, @quoted_table_names = {}, {}
-        # connect
+        @visitor = BindSubstitution.new(self)
       end
 
       ##
@@ -65,12 +140,16 @@ module ActiveRecord
         @connection.opened?
       end
 
-      def adapter_name #:nodoc:
+      def adapter_name
         ADAPTER_NAME
       end
 
-      def columns(table_name, name = nil) #:nodoc:
-        sql = "SELECT * FROM columns WHERE table_name = #{quote_column_name(table_name)} AND table_schema = #{quote_column_name(schema_name)}"
+      def add_index(table_name, column_name, options = {})
+        #noop
+      end
+
+      def columns(table_name, name = nil)
+        sql = "SELECT column_name, data_type, column_default, is_nullable FROM v_catalog.columns WHERE table_name = #{quote_column_name(table_name)} AND table_schema = #{quote_column_name(schema_name)}"
 
         columns = []
 
@@ -95,28 +174,27 @@ module ActiveRecord
       def execute(sql, name=nil)
         log(sql,name) do
           if block_given?
-            @connection = ::Vertica.connect(@connection.options)
-            @connection.query(sql) {|row| yield row }
-            @connection.close
+            @connection.query(sql) { |row| yield row }
           else
-            @connection = ::Vertica.connect(@connection.options)
-            results = @connection.query(sql)
-            @connection.close
-            results
+            @connection.query(sql)
           end
         end
       end
+
+      def native_database_types
+        NATIVE_DATABASE_TYPES
+      end 
 
       def primary_key(table)
         ''
       end
 
       ## QUOTING
-      def quote_column_name(name) #:nodoc:
+      def quote_column_name(name)
         "'#{name}'"
       end
 
-      def quote_table_name(name) #:nodoc:
+      def quote_table_name(name)
         if schema_name.blank?
           name
         else
@@ -130,28 +208,66 @@ module ActiveRecord
         @connection.reset_connection
       end
 
+      def remove_index(table_name, options = {})
+        # no-op in vertica 
+      end
+
+      def remove_index!(table_name, index_name)
+        # no-op in vertica 
+      end
+
+      def rename_index(table_name, old_name, new_name)
+        # no-op in vertica 
+      end
+
       def reset
         reconnect!
       end
 
       def schema_name
-        @schema ||= @connection.options[:schema]
+        @schema = @schema || @connection.options[:schema] || "public"
       end
 
       def select(sql, name = nil, binds = [])
         rows = []
-        @connection = ::Vertica.connect(@connection.options)
-        @connection.query(sql) {|row| rows << row }
-        @connection.close
+        @connection.query(sql) { |row| rows << row }
         rows
       end
 
-      def tables(name = nil) #:nodoc:
-        sql = "SELECT * FROM tables WHERE table_schema = #{quote_column_name(schema_name)}"
+      # Returns an array of arrays containing the field values.
+      # Order is the same as that returned by +columns+.
+      def select_rows(sql, name = nil)
+        res = execute(sql, name)
+        res.map(&:values)
+      end
+
+      def tables(name = nil)
+        sql = "SELECT table_name FROM v_catalog.tables WHERE table_schema = '#{schema_name}'"
 
         tables = []
         execute(sql, name) { |field| tables << field[:table_name] }
         tables
+      end
+
+      def table_exists?(name)
+        name          = name.to_s
+        schema, table = name.split('.', 2)
+
+        unless table # A table was provided without a schema
+          table  = schema
+          schema = nil
+        end
+
+        if name =~ /^"/ # Handle quoted table names
+          table  = name
+          schema = nil
+        end
+
+        @connection.query(<<-SQL).first[0].to_i > 0
+          SELECT COUNT(*)
+          FROM v_catalog.tables
+          WHERE table_name = '#{table.gsub(/(^"|"$)/,'')}'
+        SQL
       end
     end  
   end
