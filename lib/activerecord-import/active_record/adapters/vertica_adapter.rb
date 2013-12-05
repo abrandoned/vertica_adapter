280bddc9543d8ab072387e5f97d57dc9e328663f
Brian Stien <brian.stien@moneydesktop.com>
2013-12-05 14:05:15 -0700

    chmod 664, this file is not executable.

--- lib/activerecord-import/active_record/adapters/vertica_adapter.rb
+++ lib/activerecord-import/active_record/adapters/vertica_adapter.rb
@@ -2,5 +2,5 @@ require "active_record/connection_adapters/vertica_adapter"
 require "activerecord-import/adapters/vertica_adapter"
 
 class ActiveRecord::ConnectionAdapters::VerticaAdapter
-  include ActiveRecord::Import::VerticaAdapter::InstanceMethods
+  include ActiveRecord::Import::VerticaAdapter
 end
